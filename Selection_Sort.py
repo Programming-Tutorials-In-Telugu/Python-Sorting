fcabd1757b0422eeeb7c8003bcba233d6edd6ed9
Abhishek Kotha <abhishekkotha96180@gmail.com>
2025-01-25 16:42:40 +0530

    Selection Sort Example

---
+++ Selection_Sort.py
@@ -0,0 +1,14 @@
+def selection_sort(array):
+    n = len(array)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if array[j] < array[min_index]:
+                min_index = j
+        array[i], array[min_index] = array[min_index], array[i]
+        print("after", i + 1, "iteration", array)
+    return array
+
+
+sorted_array = selection_sort([3, 4, 1, 2, 5, 8, 6, 9, 7])
+print(sorted_array)
